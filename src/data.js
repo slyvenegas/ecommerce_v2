fd84129c92b33e070cb49bfaef9b0a02619cb590
slyvenegas <venegas@gmail.com>
2024-06-08 11:41:03 -0500

    ajustes hoja de estilos

--- src/data.js
+++ src/data.js
@@ -1,105 +1,105 @@
 export const data = [
     {
         id: 1,
-        nameProduct: 'Lapicero FC / Golg 18k',
+        nameProduct: 'CARBON FC / GOLD 18k',
         price: 180000 ,
         urlImage: 'https://almacenparker.com/wp-content/uploads/2023/06/Boligrafo-parker-im-metal-gt-2.jpg',
         quantity: 1
     },
     {
         id: 2,
-        nameProduct: 'Arrow Steel / Gel',
+        nameProduct: 'ARROW STEEL / GEL',
         price: 130000 ,
         urlImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtCxpT8NqjIb5eErVamEqdEoH0hAkPAk0ANA&s',
         quantity: 1
     },
     {
         id: 3,
-        nameProduct: 'Pluma 1mm / Gold 18k',
+        nameProduct: 'PEN 1mm / GOLD 18K',
         price: 185000 ,
         urlImage: 'https://m.media-amazon.com/images/I/51XEFBaVcqL.jpg',
         quantity: 1
     },
     {
         id: 4,
-        nameProduct: 'Rubedo Pen / The Alchemist',
+        nameProduct: 'RUBEDO PEN / THE ALCHEMIST',
         price: 290000 ,
         urlImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRJb0w-Pf0MJKD8NiCMF3yhXez6G8mjEerUg&s',
         quantity: 1
     },
     {
         id: 5,
-        nameProduct: 'Full Golg 18k / The Sun ',
+        nameProduct: 'FULL GOLD 18K / THE SUN ',
         price: 380000 ,
         urlImage: 'https://d87gtuqt6e7l.cloudfront.net/293-home_default/boligrafo-jotter-xl.jpg',
         quantity: 1
     },
     {
         id: 6,
-        nameProduct: 'Diamond Steel / Gel',
+        nameProduct: 'DIAMOND STEEL / GEL',
         price: 180000 ,
         urlImage: 'https://papeleriaeditorialzapata.com/wp-content/uploads/2023/05/3501179531847-BOLIGRAFO-PARKER-JOTTER-NEGRO-BDS-REF-1953184.png',
         quantity: 1
     },
     {
         id: 7,
-        nameProduct: 'Phantom Silver',
+        nameProduct: 'PHANTOM SILVER',
         price: 180000 ,
         urlImage: 'https://falabella.scene7.com/is/image/FalabellaCO/4288777_1?wid=800&hei=800&qlt=70',
         quantity: 1
     },
     {
         id: 8,
-        nameProduct: 'Full Metal Writer',
+        nameProduct: 'FULL METAL WRITER',
         price: 175000 ,
         urlImage: 'https://exitocol.vtexassets.com/arquivos/ids/16170681/boligrafo-en-acero-inoxidable-con-acabado-cromado-parker.jpg?v=638083911592170000',
         quantity: 1
     },
     {
         id: 9,
-        nameProduct: 'Arthur W / Gold 18k',
+        nameProduct: 'ARTHUR W / GOLD 18K',
         price: 145000 ,
         urlImage: 'https://d87gtuqt6e7l.cloudfront.net/485-large_default/boligrafo-sonnet-acero-inoxidable-gt.jpg',
         quantity: 1
     },
     {
         id: 10,
-        nameProduct: 'Middle Gold',
+        nameProduct: 'MIDDLE GOLD 1mm',
         price: 180000 ,
         urlImage: 'https://comercial-li.com.pe/cdn/shop/products/2050342313298JOTTERNEGROGT-T2_1200x1200.png?v=1620693117',
         quantity: 1
     },
     {
         id: 11,
-        nameProduct: 'Carbon Fiber Ghost',
+        nameProduct: 'CARBON FIBER GHOST',
         price: 160000 ,
         urlImage: 'https://almacenparker.com/wp-content/uploads/2023/06/IM-MONOCROMATICO-TITANIO-2172960.jpg',
         quantity: 1
     },
     {
         id: 12,
-        nameProduct: 'The Lighter / Gel',
+        nameProduct: 'THE LIGTHER / GEL',
         price: 200000 ,
         urlImage: 'https://panamericana.vtexassets.com/arquivos/ids/360302/boligrafo-parker-jotter-originals-blanco-3026981235412.jpg?v=637347527540430000',
         quantity: 1
     },
     {
         id: 13,
-        nameProduct: 'Full Gold 24K',
+        nameProduct: 'FULL GOLD 24K',
         price: 490000 ,
         urlImage: 'https://dixman.com.bo/cdn/shop/products/Boligrafo-Parker-tinta-seca-Jotter-Premium-West-End-Gold--CT-1953203_960x.jpg?v=1588796122',
         quantity: 1
     },
     {
         id: 14,
-        nameProduct: 'M-Lighter / Version mini',
+        nameProduct: 'M-LIGTHER / MINI VERSION',
         price: 120000 ,
         urlImage: 'https://panamericana.vtexassets.com/arquivos/ids/410420/boligrafo-parker-jotter-original-blanco-3026981235412.jpg?v=637687076199370000',
         quantity: 1
     },
     {
         id: 15,
-        nameProduct: 'Above-Below Writer / Gold 24k',
+        nameProduct: 'ABOVE-BELOW WRITER / GOLD 24K',
         price: 230000 ,
         urlImage: 'https://dixman.com.bo/cdn/shop/products/boligrafo-parker-tinta-seca-jotter-premium-bond-street-black-gt-1953202_960x.jpg?v=1588796113',
         quantity: 1
